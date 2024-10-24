26cf55b84bbfcf0f43cef2900f845b9f6153413c
NotNBL <nabilrabbani2000@gmail.com>
2024-10-25 00:14:57 +0700

    new database

--- database/concert_event_system.sql
+++ database/concert_event_system.sql
@@ -30,7 +30,11 @@ CREATE TABLE IF NOT EXISTS `email_reset_tokens` (
   UNIQUE KEY `token` (`token`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table concert_event_system.email_reset_tokens: ~2 rows (approximately)
+DELETE FROM `email_reset_tokens`;
+INSERT INTO `email_reset_tokens` (`id`, `email`, `token`, `created_at`, `expiration_time`) VALUES
+	(7, 'JohnThor@gmail.com', 'f7d07f68b04027b601d76654bd83a6bf', '2024-10-21 15:47:49', '2024-10-21 09:17:49'),
+	(8, 'JohnThor@gmail.com', 'af80566d2ef127d5a0183aea05efe8e1', '2024-10-21 15:49:54', '2024-10-21 09:19:54');
 
 -- Dumping structure for table concert_event_system.events
 CREATE TABLE IF NOT EXISTS `events` (
@@ -48,7 +52,11 @@ CREATE TABLE IF NOT EXISTS `events` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table concert_event_system.events: ~2 rows (approximately)
+DELETE FROM `events`;
+INSERT INTO `events` (`id`, `name`, `date`, `time`, `location`, `image_path`, `status`, `max_participants`, `current_participants`, `created_at`, `description`) VALUES
+	(16, 'Valorant', '2024-10-25', '21:54:00', 'Rumah fahry', 'uploads/events/671a6457a3960.png', 'open', 5, 1, '2024-10-24 14:54:12', 'asdwa'),
+	(18, 'mik', '2024-10-23', '21:58:00', 'Rumah fahry', 'uploads/events/671a608bf05b5.png', 'open', 5, 2, '2024-10-24 14:58:19', 'adsd');
 
 -- Dumping structure for table concert_event_system.registrations
 CREATE TABLE IF NOT EXISTS `registrations` (
@@ -62,9 +70,13 @@ CREATE TABLE IF NOT EXISTS `registrations` (
   KEY `event_id` (`event_id`),
   CONSTRAINT `registrations_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `registrations_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table concert_event_system.registrations: ~1 rows (approximately)
+DELETE FROM `registrations`;
+INSERT INTO `registrations` (`id`, `user_id`, `event_id`, `registration_date`, `status`) VALUES
+	(26, 8, 18, '2024-10-24 15:51:11', 'confirmed'),
+	(30, 9, 16, '2024-10-24 15:51:51', 'confirmed');
 
 -- Dumping structure for table concert_event_system.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -77,9 +89,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   `profile_picture` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table concert_event_system.users: ~2 rows (approximately)
+DELETE FROM `users`;
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `created_at`, `profile_picture`) VALUES
+	(4, 'Admin', 'admin@gmail.com', '$2y$10$Q6.28ikVA6wgYAzrsGF0iOYOb5QM8lZt0OxqFDiyT9pjZVqCVbSWW', 'admin', '2024-10-22 05:13:47', '../uploads/profile_pictures/671a5d945e1b8_Screenshot 2023-03-04 082647.png'),
+	(8, 'John Thor', 'JohnThor@gmail.com', '$2y$10$fCB6neqFHsE9ugjKqnm83Ob5PhjWeso8PfBWD/trcQW2JKx1AefEi', 'user', '2024-10-24 15:09:33', '../uploads/profile_pictures/671a6364ed360_modicon.png'),
+	(9, 'John Doe', 'JohnDoe@gmail.com', '$2y$10$BOyHwxJZ7S7QdkxujjO9RO2EDxFQhpXE86hYOraqi3YoE0hVRPI.G', 'user', '2024-10-24 15:44:01', NULL);
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
