afac099c96ab81850e5cb305340c27b7b2565f64
rynesx <ryanerlanda11@gmail.com>
2024-10-25 16:22:09 +0700

    p

--- database/concert_event_system.sql
+++ database/concert_event_system.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS `email_reset_tokens` (
   UNIQUE KEY `token` (`token`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table concert_event_system.email_reset_tokens: ~0 rows (approximately)
 
 -- Dumping structure for table concert_event_system.events
 CREATE TABLE IF NOT EXISTS `events` (
@@ -48,7 +48,12 @@ CREATE TABLE IF NOT EXISTS `events` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table concert_event_system.events: ~4 rows (approximately)
+INSERT INTO `events` (`id`, `name`, `date`, `time`, `location`, `description`, `max_participants`, `current_participants`, `status`, `image_path`, `created_at`) VALUES
+	(15, 'Forestra', '2024-12-31', '15:00:00', 'Orchid Forest Cikole, Lembang, Bandung', 'Konser orkestra yang unik di tengah hutan pinus. Tahun ini, acara tersebut menampilkan Erwin Gutawa Orchestra bersama musisi populer seperti Nadin Amizah, Efek Rumah Kaca, The Adams, Scaller, Isyana Sarasvati, dan Jason Ranti. Berlangsung mulai pukul 11 siang, Forestra juga memiliki tiga area berbeda: Simfoni Area untuk konser utama, Gema Area yang menampilkan toko pop-up vinyl dan diskusi inspiratif, serta Rasa Area yang menawarkan berbagai pilihan makanan dan minuman', 1000, 0, 'open', 'uploads/events/671b27ac32900.jpg', '2024-10-25 05:07:56'),
+	(16, 'Sunset di Kebun', '2025-07-23', '16:00:00', 'Kebun Raya Cibodas', 'Konser dengan konsep intimate show, penonton dapat menikmati alunan musik dari musisi seperti Nadin Amizah, Fiersa Besari, Fourtwnty, Kunto Aji, dan Reality Club.', 500, 0, 'open', 'uploads/events/671b290bc6f8f.jpg', '2024-10-25 05:13:47'),
+	(17, 'Java Jazz Festival', '2025-05-20', '18:00:00', 'JIExpo Kemayoran, Jakarta.', 'The Event lineup include Laufey, a Grammy Award-winning Icelandic jazz singer-songwriter; October London, a soulful artist signed by Snoop Dogg&#039;s Death Row Records; and The Yussef Dayes Experience, known for its boundary-pushing jazz. Also featured are Randy Brecker, legendary trumpeter, and groups like Scary Pockets, The Amy Winehouse Band, and Incognito, who will return with their 19th album. The festival also highlights Indonesian talents such as Project Pop, Ardhito Pramono, Tompi, Maliq &amp; D’Essentials, and many more', 10000, 0, 'open', 'uploads/events/671b2ab92eed5.jpg', '2024-10-25 05:20:57'),
+	(18, 'Knotfest festival', '2025-09-21', '18:30:00', 'Waterworks Park in Des Moines, Iowa.', 'Knotfest festival line up Des Moines, Iowa, this year&#039;s festival will feature Slipknot as the headliner, joined by an impressive roster, including Till Lindemann (Rammstein), Knocked Loose, Hatebreed, Poison the Well, and GWAR. Additional bands include Vended, Dying Wish, Zulu, Twin Temple, Holy Wars, Swollen Teeth, Spine, and Dose, adding diversity to the heavy metal lineup.', 7000, 0, 'open', 'uploads/events/671b2d5880a15.jpg', '2024-10-25 05:32:08');
 
 -- Dumping structure for table concert_event_system.registrations
 CREATE TABLE IF NOT EXISTS `registrations` (
@@ -63,7 +68,7 @@ CREATE TABLE IF NOT EXISTS `registrations` (
   CONSTRAINT `registrations_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table concert_event_system.registrations: ~0 rows (approximately)
 
 -- Dumping structure for table concert_event_system.users
 CREATE TABLE IF NOT EXISTS `users` (
@@ -78,7 +83,9 @@ CREATE TABLE IF NOT EXISTS `users` (
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Data exporting was unselected.
+-- Dumping data for table concert_event_system.users: ~1 rows (approximately)
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `created_at`, `profile_picture`) VALUES
+	(2, 'Admin', 'Admin@gmail.com', '$2y$10$uZVYNwibVe48nCTab4NsR.7p15RuZ1dHN/srKybdvNkoqnFEl74Va', 'admin', '2024-10-22 03:23:55', '../uploads/profile_pictures/671b2c09ccc3b_pp.png');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
